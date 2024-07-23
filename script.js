be74410df6ee97fd67774558a12a00bbd541b8d4
pauline <lanquetin.pauline@gmail.com>
2024-07-23 10:51:58 -0600

    Fix mobile navbar

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+var navbar = document.querySelector('.navbar');
+var coverPhoto = document.querySelector('.cover-photo');
+var coverPhotoHeight = coverPhoto.getBoundingClientRect().height;
+
+if (window.innerWidth < 992 && window.scrollY < coverPhotoHeight) {
+    navbar.style.display = 'none';
+}
+
 window.addEventListener('scroll', function() {
-    var coverPhoto = document.querySelector('.cover-photo');
-    var navbar = document.querySelector('.navbar');
     var navbarMenusList = navbar.querySelector('.navbar-nav');
     var navbarLinks = navbar.querySelectorAll('.nav-link');
     var navbarToggler = navbar.querySelector('.custom-toggler');
@@ -8,53 +14,52 @@ window.addEventListener('scroll', function() {
     var buttonsWhite = navbar.querySelectorAll('.btn-white');
 
     // Récupérer la hauteur de la photo de couverture
-    var coverPhotoHeight = coverPhoto.getBoundingClientRect().height;
     var navbarHeight = navbar.getBoundingClientRect().height;
 
-    //if (window.innerWidth >= 992) {
-        // Vérifier si l'utilisateur a fait défiler jusqu'au bas de la photo de couverture
-        if ((window.scrollY >= (coverPhotoHeight*0.2 - navbarHeight)) && (window.scrollY < coverPhotoHeight*0.8 - navbarHeight)) {
-            setTimeout(function() {
-                navbarMenusList.style.visibility = 'hidden';
-                navbarToggler.style.visibility = 'hidden';
-            }, 40);
-
-        }
-        else if (window.scrollY >= coverPhotoHeight - navbarHeight) {
-            setTimeout(function() {
-                navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-                navbar.style.backdropFilter = 'blur(10px)';
-                navbarMenusList.style.visibility = 'visible';
-                navbarToggler.style.visibility = 'visible';
-                navbarDate.style.color = 'black';
-                for(var i=0 ; i<navbarLinks.length ; i++) {
-                    navbarLinks[i].style.color = 'black';
-                }
-                for(var i=0 ; i<buttonsWhite.length ; i++) {
-                    buttonsWhite[i].style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-                    buttonsWhite[i].style.color = 'white';
-                }
-            }, 40);
-        }
-        else {
-            setTimeout(function() {
-                navbar.style.backgroundColor = 'rgba(255, 255, 255, 0)';
-                navbar.style.backdropFilter = 'blur(0px)';
-                navbarMenusList.style.visibility = 'visible';
-                navbarToggler.style.visibility = 'visible';
-                navbarDate.style.color = 'white';
-                for(var i=0 ; i<navbarLinks.length ; i++) {
-                    navbarLinks[i].style.color = 'white';
-                }
-                for(var i=0 ; i<buttonsWhite.length ; i++) {
-                    buttonsWhite[i].style.backgroundColor = 'white';
-                    buttonsWhite[i].style.color = 'black';
-                }
-            }, 40);
-                
-        }
-    //}
-        
+    if (window.innerWidth < 992 && (window.scrollY >= (coverPhotoHeight*0.2 - navbarHeight)) && (window.scrollY < coverPhotoHeight*0.8 - navbarHeight)) {
+        navbar.style.display = 'none';
+    }
+    // Vérifier si l'utilisateur a fait défiler jusqu'au bas de la photo de couverture
+    else if ((window.scrollY >= (coverPhotoHeight*0.2 - navbarHeight)) && (window.scrollY < coverPhotoHeight*0.8 - navbarHeight)) {
+        setTimeout(function() {
+            navbarMenusList.style.visibility = 'hidden';
+            navbarToggler.style.visibility = 'hidden';
+        }, 40);
+    }
+    else if (window.scrollY >= coverPhotoHeight - navbarHeight) {
+        setTimeout(function() {
+            navbar.style.display = 'flex';
+            navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+            navbar.style.backdropFilter = 'blur(10px)';
+            navbarMenusList.style.visibility = 'visible';
+            navbarToggler.style.visibility = 'visible';
+            navbarDate.style.color = 'black';
+            for(var i=0 ; i<navbarLinks.length ; i++) {
+                navbarLinks[i].style.color = 'black';
+            }
+            for(var i=0 ; i<buttonsWhite.length ; i++) {
+                buttonsWhite[i].style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+                buttonsWhite[i].style.color = 'white';
+            }
+        }, 40);
+    }
+    else {
+        setTimeout(function() {
+            //navbar.style.display = 'flex';
+            navbar.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+            navbar.style.backdropFilter = 'blur(0px)';
+            navbarMenusList.style.visibility = 'visible';
+            navbarToggler.style.visibility = 'visible';
+            navbarDate.style.color = 'white';
+            for(var i=0 ; i<navbarLinks.length ; i++) {
+                navbarLinks[i].style.color = 'white';
+            }
+            for(var i=0 ; i<buttonsWhite.length ; i++) {
+                buttonsWhite[i].style.backgroundColor = 'white';
+                buttonsWhite[i].style.color = 'black';
+            }
+        }, 40);         
+    }        
 });
 
 let imageReceptionNumber = 1;
